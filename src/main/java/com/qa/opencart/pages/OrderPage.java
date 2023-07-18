f1c1a40279350cabb6d3696640491051882b95b0
Anshuman Sehgal <anshumansehgal@Anshumans-MacBook-Pro.local>
2023-07-18 15:11:54 -0400

    Added order page

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class OrderPage {
+	
+	By location = By.id("location");
+	
+	public void getOrder() {
+		System.out.println("Order page--get order");
+	}
+
+}
