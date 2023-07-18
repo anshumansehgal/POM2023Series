79d73fc6bcf02363ad8b020d8b456c6d15c14b6c
Anshuman Sehgal <anshumansehgal@Anshumans-MacBook-Pro.local>
2023-07-18 16:24:01 -0400

    updated order page as per comment

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -5,9 +5,13 @@ import org.openqa.selenium.By;
 public class OrderPage {
 	
 	By location = By.id("location");
+	By proce = By.id("price");
 	
 	public void getOrder() {
 		System.out.println("Order page--get order");
 	}
 
+	public void getPrice() {
+		System.out.println("Order page--get price");
+	}
 }
