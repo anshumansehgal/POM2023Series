a5d775b34e877bcd6903b355e55ed656b2b6c6ed
Anshuman Sehgal <anshumansehgal@Anshumans-MacBook-Pro.local>
2023-07-26 16:19:02 -0400

    Added code for Remote WebDriver

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 import org.aspectj.util.FileUtil;
@@ -13,22 +15,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import com.qa.opencart.exception.FrameworkException;
 
-
 public class DriverFactory {
-	
+
 	public WebDriver driver;
 	public Properties prop;
 	public OptionsManager optionsManager;
-	
+
 	public static String highlight;
-	
+
 	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
-	
-	
+
 	/**
 	 * This method is initializing the driver on the basis of given browser name.
 	 * @param browserName
@@ -43,20 +44,48 @@ public class DriverFactory {
 		
 		System.out.println("Browser name is " + browserName);
 		
+		//Chrome
 		if(browserName.equalsIgnoreCase("chrome")) {
-			//driver = new ChromeDriver(optionsManager.getChromeOptions());
-			tlDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
+			
+			//Hub properties from config
+			if(Boolean.parseBoolean(prop.getProperty("remote"))){
+				//Run on remote/selenium grid
+				init_remoteDriver("chrome");
+			}
+			else {
+				//local execution
+				//driver = new ChromeDriver(optionsManager.getChromeOptions());
+				tlDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
+			}
 		}
+		//Firefox
 		else if(browserName.equalsIgnoreCase("firefox")) {
+			
+			if(Boolean.parseBoolean(prop.getProperty("remote"))){
+				//Run on remote/selenium grid
+				init_remoteDriver("firefox");
+			}
+			else {
 			//driver = new FirefoxDriver(optionsManager.getFirefoxOptions());
 			tlDriver.set(new FirefoxDriver(optionsManager.getFirefoxOptions()));
+			}
 		}
+		//Edge
+		else if(browserName.equalsIgnoreCase("edge")) {
+			
+			if(Boolean.parseBoolean(prop.getProperty("remote"))){
+				init_remoteDriver("edge");
+			}
+			else {
+				//driver = new FirefoxDriver(optionsManager.getFirefoxOptions());
+				tlDriver.set(new EdgeDriver(optionsManager.getEdgeOptions()));
+			}
+		}
+		//Safari doesn't support remote execution so only local
 		else if(browserName.equalsIgnoreCase("safari")) {
 			driver = new SafariDriver();
 		}
-		else if(browserName.equalsIgnoreCase("edge")) {
-			driver = new EdgeDriver();
-		}
+		
 		else {
 			System.out.println("Please pass right browser name..." + browserName);
 		}
@@ -66,85 +95,113 @@ public class DriverFactory {
 		getDriver().get(prop.getProperty("url").trim());
 		return getDriver();
 	}
-	
-	//synchronized--every driver will get its respective copy. Get the local thread copy of the driver. 
-	public synchronized static WebDriver getDriver() {
-		return tlDriver.get();	
+
+	/**
+	 * This method is called internally to initialize the driver with RemoteWebDriver
+	 * @param browser
+	 */
+	private void init_remoteDriver(String browser) {
+
+		System.out.println("Running tests on Grid Server:: " + browser);
+		
+		try {
+			switch (browser.toLowerCase()) {
+			case "chrome":
+				tlDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getChromeOptions()));
+				break;
+			case "firefox":
+				tlDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getFirefoxOptions()));
+				break;
+			case "edge":
+				tlDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getEdgeOptions()));
+				break;
+			default:
+				System.out.println("Please pass the right browser for remote execution.." + browser);
+				throw new FrameworkException("NOREMOTEBROWSEREXCEPTION");
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
 	}
-	
+
+	// synchronized--every driver will get its respective copy. Get the local thread
+	// copy of the driver.
+	public synchronized static WebDriver getDriver() {
+		return tlDriver.get();
+	}
+
 	/**
 	 * This method is reading the properties from .properties file.
+	 * 
 	 * @return
 	 */
 	public Properties initProperty() {
-		
-		/* command line argument
-		 * mvn clean install -Denv="dev"
-		 * mvn clean install -->what is the value of env in this case? null --so we should be putting a null check
+
+		/*
+		 * command line argument mvn clean install -Denv="dev" mvn clean install -->what
+		 * is the value of env in this case? null --so we should be putting a null check
 		 */
-		
-		//create obj of properties
+
+		// create obj of properties
 		prop = new Properties();
-		FileInputStream ip=null;
+		FileInputStream ip = null;
 		String envName = System.getProperty("env");
-		System.out.println("Running test cases on: " +envName);
-		
+		System.out.println("Running test cases on: " + envName);
+
 		try {
-		if(envName == null) {
-			System.out.println("No environment is passed...Running test on qa env");
-			 ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
-		}
-		else {
-			
-			switch (envName.toLowerCase().trim()) {
-			case "qa":
-				 ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
-				break;
-			case "stage":
-				 ip = new FileInputStream("./src/test/resources/config/stag.config.properties");
-				break;
-			case "dev":
-				 ip = new FileInputStream("./src/test/resources/config/dev.config.properties");
-				break;
-			case "prod":
-				 ip = new FileInputStream("./src/test/resources/config/config.properties");
-				break;
-			default:
-				System.out.println("Wrong env is passed...No need to run the test cases.");
-				throw new FrameworkException("INCORRECT ENV IS PASSED....");
-				//break;
+			if (envName == null) {
+				System.out.println("No environment is passed...Running test on qa env");
+				ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
+			} else {
+
+				switch (envName.toLowerCase().trim()) {
+				case "qa":
+					ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
+					break;
+				case "stage":
+					ip = new FileInputStream("./src/test/resources/config/stag.config.properties");
+					break;
+				case "dev":
+					ip = new FileInputStream("./src/test/resources/config/dev.config.properties");
+					break;
+				case "prod":
+					ip = new FileInputStream("./src/test/resources/config/config.properties");
+					break;
+				default:
+					System.out.println("Wrong env is passed...No need to run the test cases.");
+					throw new FrameworkException("INCORRECT ENV IS PASSED....");
+				// break;
+				}
 			}
+		} catch (FileNotFoundException e) {
+
 		}
-		}
-		catch(FileNotFoundException e) {
-			
-		}
-		//Load the prop.
+		// Load the prop.
 		try {
 			prop.load(ip);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return prop;
 	}
-	
+
 	/**
 	 * This method is used to take screenshot
 	 */
 	public static String getScreenshot() {
-		File srcFile = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
+		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
 		String path = System.getProperty("user.dir") + "/screenshots/" + System.currentTimeMillis() + ".png";
 		File destination = new File(path);
 		try {
 			FileUtil.copyFile(srcFile, destination);
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
 		}
 		return path;
-		
+
 	}
-	
 
 }
--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -3,6 +3,7 @@ package com.qa.opencart.factory;
 import java.util.Properties;
 
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class OptionsManager {
@@ -10,6 +11,7 @@ public class OptionsManager {
 	private Properties prop;
 	private ChromeOptions co;
 	private FirefoxOptions fo;
+	private EdgeOptions eo;
 
 	// How to supply prop to above guy? Creating const. of this class
 	public OptionsManager(Properties prop) {
@@ -49,4 +51,19 @@ public class OptionsManager {
 		return fo;
 	}
 
+	// Edge Options
+	public EdgeOptions getEdgeOptions() {
+			eo = new EdgeOptions();
+
+			if (Boolean.parseBoolean(prop.getProperty("headless").trim())) {
+				System.out.println("Running edge in headless");
+				eo.addArguments("--headless");
+			}
+				
+			if (Boolean.parseBoolean(prop.getProperty("incognito").trim())) {
+				System.out.println("Running edge in incognito");
+				eo.addArguments("--incognito");
+			}
+			return eo;
+		}
 }
